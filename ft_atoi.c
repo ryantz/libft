86d7829bba286f720ea9898dc13963907e0032d6
ryantz <ryantanzhi1997@gmail.com>
2025-11-12 22:00:29 +0800

    finished atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,23 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 22:36:15 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/09 23:06:52 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/12 22:00:20 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// TODO: ignore space, take into account neg, pos, /t/a/b...
+int	ft_isspace(const char *nptr)
+{
+	int	offset;
+
+	offset = 0;
+	while (nptr[offset] == ' '
+		|| (nptr[offset] >= 9 && nptr[offset] <= 13))
+		offset++;
+	return (offset);
+}
+
 int	ft_ten_power(int val, int times)
 {
 	int	i;
@@ -26,29 +36,47 @@ int	ft_ten_power(int val, int times)
 	return (val);
 }
 
+int	ft_strlen_only_numbers(const char *nptr)
+{
+	int	len;
+
+	len = 0;
+	while (nptr[len] && (nptr[len] >= '0' && nptr[len] <= '9'))
+		len++;
+	return (len);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	long unsigned int	len;
-	int	res;
-	int	times;
+	long unsigned int	offset;
+	int					res;
+	int					times;
+	int					sign;
 
 	res = 0;
 	times = 0;
-	len = ft_strlen(nptr);
+	sign = 0;
+	offset = ft_isspace(nptr);
+	if (nptr[offset] == '-' || nptr[offset] == '+')
+		sign = 1;
+	len = ft_strlen_only_numbers(nptr + offset + sign);
 	while (len > 0)
 	{
-		res += ft_ten_power((nptr[len - 1] - '0'), times);
+		res += ft_ten_power((nptr[offset + len - (!sign)] - '0'), times);
 		times++;
 		len--;
 	}
+	if (nptr[offset] == '-')
+		res *= -1;
 	return (res);
 }
 
-#include <stdlib.h>
-#include <stdio.h>
+/*
 int	main(void)
 {
-	const char *nptr = "1234";
+	const char *nptr = "         \t\n\v\f\r01234abc21";
 	printf("%d\n", atoi(nptr));
 	printf("%d\n", ft_atoi(nptr));
 }
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 16:30:54 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/09 22:57:54 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/12 21:33:17 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
+# include <stdio.h> // remove
 
 size_t	ft_strlen(const char *s);
 int	ft_tolower(int c);
