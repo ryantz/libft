988f0a2178fd8422d5ab07ccdb75b616a356e6c0
ryantz <ryantanzhi1997@gmail.com>
2025-11-14 22:35:18 +0800

    ft_isprint done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 16:30:54 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/14 22:27:47 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/14 22:34:55 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@ int	ft_strcmp(char *s1, char *s2);
 int	ft_isdigit(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
 int	ft_isalnum(int c);
+
 int	ft_tolower(int c);
 int	ft_toupper(int c);
 
