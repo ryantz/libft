890d2529a35d7ce14767e64f9e2168b97908e14c
ryantz <ryantanzhi1997@gmail.com>
2025-11-14 22:28:04 +0800

    ft_isascii done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 16:30:54 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/14 21:49:34 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/14 22:27:47 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@
 # include <ctype.h> // remove
 
 size_t	ft_strlen(const char *s);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
 int	ft_strcmp(char *s1, char *s2);
+
 int	ft_isdigit(int c);
 int	ft_isalpha(int c);
+int	ft_isascii(int c);
 int	ft_isalnum(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 #endif
