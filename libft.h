ff0e569da664cb3b931ec5c09216ac9e82164c04
ryantz <ryantanzhi1997@gmail.com>
2025-11-14 21:27:57 +0800

    ft_isalpha done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 16:30:54 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/12 21:33:17 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/14 21:22:30 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <unistd.h>
 # include <string.h>
 # include <stdio.h> // remove
+# include <ctype.h> // remove
 
 size_t	ft_strlen(const char *s);
 int	ft_tolower(int c);
