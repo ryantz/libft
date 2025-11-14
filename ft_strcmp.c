3cd6424afe90df578210f0dbe0a2bf8940bd90ca
ryantz <ryantanzhi1997@gmail.com>
2025-11-14 21:17:23 +0800

    strcmp done

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 18:18:34 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/09 18:20:21 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/14 21:17:04 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,20 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] || s2[i])
 	{
-		if (s1[i] == s2[i])
-		{
-		}
-		else
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
+
+/*
+int	main(void)
+{
+	char *str1 = "hello";
+	char *str2 = "helli";
+
+	printf("%d\n", ft_strcmp(str1, str2));
+	printf("%d\n", strcmp(str1, str2));
+}
+*/
