6b02c3368793a174099566c2ea4f80f37d7a088d
ryantz <ryantanzhi1997@gmail.com>
2025-11-14 21:55:46 +0800

    ft_tolower done

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 18:15:02 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/09 18:16:28 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/14 21:55:33 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,31 @@
 
 int	ft_tolower(int c)
 {
-	return (c + 32);
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
 }
+
+/*
+int	main(void)
+{
+	int	test_cap;
+	int	test_noncap;
+	int	res;
+	
+	test_cap = 'A';
+	test_noncap = 'a';
+	while (test_cap <= 'Z')
+	{
+		res = ft_tolower(test_cap);
+		printf("%c\n", res);
+		test_cap++;
+	}
+	while (test_noncap <= 'z')
+	{
+		res = ft_tolower(test_noncap);
+		printf("%c\n", res);
+		test_noncap++;
+	} 
+}
+*/
