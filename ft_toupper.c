46ccf7c7f9038ffd93bf979c581154bfa94e7ad6
ryantz <ryantanzhi1997@gmail.com>
2025-11-14 21:58:37 +0800

    ft_toupper done

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 18:16:48 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/09 18:17:15 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/14 21:58:04 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,31 @@
 
 int	ft_toupper(int c)
 {
-	return (c - 32);
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
 }
+
+/*
+int	main(void)
+{
+	int	test_cap;
+	int	test_noncap;
+	int	res;
+	
+	test_cap = 'A';
+	test_noncap = 'a';
+	while (test_cap <= 'Z')
+	{
+		res = ft_toupper(test_cap);
+		printf("%c\n", res);
+		test_cap++;
+	}
+	while (test_noncap <= 'z')
+	{
+		res = ft_toupper(test_noncap);
+		printf("%c\n", res);
+		test_noncap++;
+	} 
+}
+*/
