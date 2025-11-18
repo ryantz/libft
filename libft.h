56a1c7121298d96621a015794e209367c15dcb20
ryantz <ryantanzhi1997@gmail.com>
2025-11-18 20:15:47 +0800

    .

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 16:30:54 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/15 10:05:29 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/18 20:14:46 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,29 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <stddef.h> // size_t
-# include <string.h> // remove
-# include <strings.h> // remove
+# include <bsd/string.h>
 # include <stdio.h> // remove
-# include <ctype.h> // remove
 
 void	ft_bzero(void *s, size_t n);
 
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_isalnum(int c);
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+
 size_t	ft_strlen(const char *s);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
+char	*ft_strstr(const char *haystack, const char *needle);
 
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_isalnum(int c);
 
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
-int	ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
 
 #endif
