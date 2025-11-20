e8cf7e4803f4d7e245326c703d51239c3fc1ac6d
ryantz <ryantanzhi1997@gmail.com>
2025-11-20 22:50:47 +0800

    ft_memchr done.

---
+++ ft_memchr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 22:40:30 by ryatan            #+#    #+#             */
+/*   Updated: 2025/11/20 22:50:06 by ryatan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*dp_s;
+
+	dp_s = (char *)s;
+	i = 0;
+	while (dp_s[i] && i < n)
+	{
+		if (dp_s[i] == c)
+			return (&dp_s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+//int	main(void)
+//{
+//	const char *s1 = "hello there stranger";
+//	int	c = 'a';
+//	size_t n = 16;
+//	char	*s;
+//	char	*s2;
+//
+//	s = memchr(s1, c, n);
+//	s2 = ft_memchr(s1, c, n);
+//	printf("memchr:%s\n", s);
+//	printf("ft_memchr:%s\n", s2);
+//}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 16:30:54 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/19 21:55:15 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/20 22:50:26 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int		ft_isalnum(int c);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
