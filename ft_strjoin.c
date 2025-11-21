98ab216d77ebd1b7db2b0b856b823230270d8250
Ryan Zhiyang Tan <ryatan@c2r2s4.42singapore.sg>
2025-11-21 15:50:50 +0800

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ryatan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 15:37:41 by ryatan            #+#    #+#             */
-/*   Updated: 2025/11/21 15:38:17 by ryatan           ###   ########.fr       */
+/*   Updated: 2025/11/21 15:50:33 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,40 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	*p_strjoin;
+
+	len = ft_strlen(s1) + ft_strlen(s2) - 2;
+	p_strjoin = malloc(sizeof(char) * len + 1);
+	i = 0;
+	j = 0;
+	while (s1[j])
+	{
+		p_strjoin[i] = s1[j];
+		j++;
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		p_strjoin[i] = s2[j];
+		j++;
+		i++;
+	}
+	p_strjoin[i] = '\0';
+	return (p_strjoin);
 }
 
-int	main(void)
-{
-	return (0);
-}
+//int	main(void)
+//{
+//	char const *s1 = "hello ";
+//	char const *s2 = "there";
+//	char	*join;
+//
+//	join = ft_strjoin(s1, s2);
+//	printf("%s\n", join);
+//	free(join);
+//	return (0);
+//}
